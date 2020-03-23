61bc818985bc04a4291c6956151d7f5e6286492b
Q <50916196+QHman@users.noreply.github.com>
2020-03-23 15:07:06 -0400

    Update Project1_part1.py

--- Project1_part1.py
+++ Project1_part1.py
@@ -3,4 +3,21 @@ import numpy as np
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 %matplotlib inline
-print("Hello World!")
+file = 1
+type = 0
+filetype = ("Adduser_", "Hydra_FTP_", "Hydra_SSH_", "Java_Meterpreter_", "Meterpeter_", "Web_Shell_")
+while(type  < 6){
+    while(file < 8){
+        f = open(filetype[type] + file, 'r')
+        while(f){
+        one =
+        two =
+        three =
+
+        }
+
+    f.close()
+    file ++
+    }
+    type ++
+}
