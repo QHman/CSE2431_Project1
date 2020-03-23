f68db7c21b78308320669e9dff193ce79e8d9a52
Q <50916196+QHman@users.noreply.github.com>
2020-03-23 15:44:55 -0400

    Update Project1_part1.py

--- Project1_part1.py
+++ Project1_part1.py
@@ -1,19 +1,28 @@
 import pandas as pd
 import numpy as np
+import os
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 %matplotlib inline
+directory = 'ADFA-LD\\ADFA-LD\\Attack_Data_Master\\'
 file = 1
 type = 0
 filetype = ("Adduser_", "Hydra_FTP_", "Hydra_SSH_", "Java_Meterpreter_", "Meterpeter_", "Web_Shell_")
 while(type  < 6){
     while(file < 8){
-        f = open(filetype[type] + file, 'r')
-        while(f){
-        one =
-        two =
-        three =
+        listing = os.listdir(path)
+        for infile in listin
+        f = open(os.path.join(path,infile),'r')
+        f = open( + filetype[type] + file + , 'r')
+        sequence = f.read()
+        calls = sequence.split()
+        one = calls[0]
+        two = calls[1]
+        three = calls[2]
+        n = 3
+        while(len(calls) > n ){
 
+        n ++
         }
 
     f.close()
