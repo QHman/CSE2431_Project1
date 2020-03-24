4654f81a98104ef4be3d0b4e476f2f24b8fb6e57
Q <50916196+QHman@users.noreply.github.com>
2020-03-24 19:22:20 -0400

    Update Project1_part1.py

--- Project1_part1.py
+++ Project1_part1.py
@@ -3,64 +3,112 @@
 import os
 # from sklearn.svm import SVC
 # import matplotlib.pyplot as plt
-file = 1
-type = 0
-numtype =  6
-numfile =  7
-seqFreq = []
-numberSeqFreq = []
-attacks = []
-filetype = ('Adduser_', 'Hydra_FTP_', 'Hydra_SSH_', 'Java_Meterpreter_', 'Meterpreter_', 'Web_Shell_')
-# Types of Attacks
-while(type  < numtype):
-    print(filetype[type])
-    file = 1
-    # Folder in type ex. 1, 2, 3
+
+def inputDataAttacks(startnum, endnumfile):
+    typeCalls = 'Attack_Data_Master'
+    startnumfile = startnum
+    seqFreq = []
+    numberSeqFreq = []
+    attacks = []
+    file = []
     folder = []
-    while(file <= numfile):
-        filenum = ''.join([filetype[type], str(file)])
-        print(filenum)
-        directory = '\\'.join(['ADFA-LD', 'ADFA-LD', 'Attack_Data_Master',filenum])
-        listing = os.listdir(directory)
-        l = 0
-        # Text Doc in folder... About 10
-        while(l < len(listing)):
-            print(listing[l])
-            f = open('\\'.join([directory,listing[l]]),'r')
-            sequence = f.read()
-            f.close()
-            calls = sequence.split()
-            print(len(calls))
-            gram = 3
-            n = gram
-            seqFreq = []
-            numberSeqFreq = []
-            # Take sequence from file, change to frequency
-            while (len(calls) >= n ):
-                k = 0
-                seq = []
-                while(k < gram):
-                    seq.append(calls[(n+k-gram)])
-                    k += 1
-                k = 0
-                passval = 0
-                # Save each frequency, per text file
-                while (k < len(seqFreq) and passval == 0):
-                    if(len(seqFreq) == 0):
+    directory = '\\'.join(['ADFA-LD', 'ADFA-LD', typeCalls])
+    filetype = ('Adduser_', 'Hydra_FTP_', 'Hydra_SSH_', 'Java_Meterpreter_', 'Meterpreter_', 'Web_Shell_')
+    # Types of Attacks
+    for files in filetype:
+        startnumfile = startnum
+        while(startnumfile <= endnumfile):
+            print(''.join([files, str(startnumfile)]))
+            directory2 = '\\'.join([directory, ''.join([files, str(startnumfile)])])
+            #   Folder in type ex. 1, 2, 3
+            folder = []
+            listing = os.listdir(directory2)
+            # Text Doc in folder... About 10
+            for l in listing:
+                f = open('\\'.join([directory2,l]),'r')
+                sequence = f.read()
+                f.close()
+                calls = sequence.split()
+                gram = 3
+                n = gram
+                seqFreq = []
+                numberSeqFreq = []
+                # Take sequence from file, change to frequency
+                while (len(calls) >= n ):
+                    k = 0
+                    seq = []
+                    while(k < gram):
+                        seq.append(calls[(n+k-gram)])
+                        k += 1
+                    k = 0
+                    passval = 0
+                    # Save each frequency, per text file
+                    while (k < len(seqFreq) and passval == 0):
+                        if(len(seqFreq) == 0):
+                            seqFreq.append(seq)
+                            numberSeqFreq.append(1)
+                        if(seqFreq[k] == seq):
+                            numberSeqFreq[k] += 1
+                            passval = 1
+                        k += 1
+                    if (passval == 0):
                         seqFreq.append(seq)
                         numberSeqFreq.append(1)
-                    if(seqFreq[k] == seq):
-                        numberSeqFreq[k] += 1
-                        passval = 1
+                    n += 1
+                file.append([seqFreq, numberSeqFreq])
+            folder.append(file)
+            file = []
+            startnumfile += 1
+            attacks.append(folder)
 
-                    k += 1
-                if (passval == 0):
+    print(attacks[0][0][0])
+    return attacks
+
+def inputDataBenign(typeCalls):
+    seqFreq = []
+    numberSeqFreq = []
+    benign = []
+    txtfile = []
+    directory = '\\'.join(['ADFA-LD', 'ADFA-LD', typeCalls])
+    filetype = os.listdir(directory)
+    # Types of Attacks
+    for type in filetype:
+        f = open('\\'.join([directory,type]),'r')
+        sequence = f.read()
+        f.close()
+        calls = sequence.split()
+        gram = 3
+        n = gram
+        seqFreq = []
+        numberSeqFreq = []
+        # Take sequence from file, change to frequency
+        txtfile = []
+        while (len(calls) >= n ):
+            k = 0
+            seq = []
+            while(k < gram):
+                seq.append(calls[(n+k-gram)])
+                k += 1
+            k = 0
+            passval = 0
+            # Save each frequency, per text file
+            while (k < len(seqFreq) and passval == 0):
+                if(len(seqFreq) == 0):
                     seqFreq.append(seq)
                     numberSeqFreq.append(1)
-                n += 1
-            l += 1
-            folder.append([seqFreq, numberSeqFreq])
-        file += 1
-    attacks.append(folder)
-    print(attacks)
-    type += 1
+                if(seqFreq[k] == seq):
+                    numberSeqFreq[k] += 1
+                    passval = 1
+                k += 1
+            if (passval == 0):
+                seqFreq.append(seq)
+                numberSeqFreq.append(1)
+            n += 1
+            txtfile.append([seqFreq, numberSeqFreq])
+        benign.append(txtfile)
+    print(benign[0][0])
+
+def
+
+inputDataAttacks(1,7)
+inputDataBenign('Training_Data_Master')
