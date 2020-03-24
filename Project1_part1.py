9a2587d2e1644c7aacc1ad6f47a5504ff1e57c09
Q <50916196+QHman@users.noreply.github.com>
2020-03-24 10:16:53 -0400

    Update Project1_part1.py

--- Project1_part1.py
+++ Project1_part1.py
@@ -3,18 +3,17 @@ import numpy as np
 import os
 from sklearn.svm import SVC
 import matplotlib.pyplot as plt
-%matplotlib inline
 file = 1
 type = 0
-seqFreq = np.array[]
-numberSeqFreq = np.array[]
-filetype = ("Adduser_", "Hydra_FTP_", "Hydra_SSH_", "Java_Meterpreter_", "Meterpeter_", "Web_Shell_")
-while(type  < 6){
-    while(file < 8){
+seqFreq = []
+numberSeqFreq = []
+filetype = ('Adduser_', 'Hydra_FTP_', 'Hydra_SSH_', 'Java_Meterpreter_', 'Meterpeter_', 'Web_Shell_')
+while(type  < 6):
+    while(file < 8):
         directory = os.join('ADFA-LD\\ADFA-LD\\Attack_Data_Master\\',filetype[type], file, '\\')
-        listing = os.listdir(directory);
+        listing = os.listdir(directory)
         l = 0
-        while(l < len(listing)){
+        while(l < len(listing)):
             f = open(os.join(directory,'\\',infile),'r')
             sequence = f.read()
             f.close()
@@ -22,35 +21,28 @@ while(type  < 6){
             print(calls)
             gram = 2
             n = gram
-            while(len(calls) >= n ){
+            while (len(calls) >= n ):
                 k = 0
                 seq = []
-                while(k < gram){
+                while(k < gram):
                     seq.append(cals[(n+k-grams)])
-                    k ++
-                }
+                    k += 1
+
+                print(seq)
                 k = 0
-                pass = 0
-                while(k < seqFreq && pass == 0){
-                    if(seqFreq[k] == seq){
-                    numberSeqFreq[k] ++
-                    pass = 1
-                    }
-                    k ++
-                }
-                if (pass == 0){
+                passval = 0
+                while (k < seqFreq and passval == 0):
+                    if(seqFreq[k] == seq):
+                        numberSeqFreq[k] += 1
+                        passval = 1
+
+                    k += 1
+
+                if (passval == 0):
                     seqFreq.append(seq)
                     numberSeqFreq.append(1)
-                }
-
-                n ++
-            }
-
-
-            l++
-        }
-
-    file ++
-    }
-    type ++
-}
+                print(seqFreq)
+                n += 1
+            l +=1
+        file +=1
+    type +=1
